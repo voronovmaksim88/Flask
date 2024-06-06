641be2f24338e3f1aadc129513a5af817fa6f243
voronovmaksim88 <voronovmaksim88@gmail.com>
2024-06-06 19:44:33 +0700

    добавил страничку

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 
 # Создаем экземпляр Flask
 app = Flask(__name__)
@@ -7,7 +7,12 @@ app = Flask(__name__)
 # Определяем маршрут для главной страницы
 @app.route('/')
 def home():
-    return "Hello, World!"
+    return render_template('index.html')
+
+
+@app.route('/about')
+def about():
+    return "<h1> О сайте </h1>"
 
 
 # Запускаем сервер, если этот файл выполнен как главный
